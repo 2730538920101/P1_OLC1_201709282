b053a5535e01f529bd9ed21bc5ec62b7060332a4
2730538920101 <2730538920101@ingenieria.usac.edu.gt>
2021-09-03 01:15:28 -0600

    CUARTO COMMIT

---
+++ src/ASTfca/Instrucciones/Variable.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ASTfca.Instrucciones;
+
+/**
+ *
+ * @author Carlos Martinez
+ */
+public class Variable {
+
+    /**
+     * @return the tipo
+     */
+    public TipoVariable getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(TipoVariable tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the valor
+     */
+    public String getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the valorp
+     */
+    public Puntaje getValorp() {
+        return valorp;
+    }
+
+    /**
+     * @param valorp the valorp to set
+     */
+    public void setValorp(Puntaje valorp) {
+        this.valorp = valorp;
+    }
+    public TipoVariable tipo;
+    public String id;
+    public String valor;
+    public Puntaje valorp;
+    public Variable(TipoVariable tipo, String id, String valor){
+        this.tipo = tipo;
+        this.id = id;
+        this.valor = valor;
+    }
+    public Variable(TipoVariable tipo, Puntaje valorp){
+        this.tipo = tipo;
+        this.id = id;
+        this.valorp = valorp;
+    }
+    
+}
